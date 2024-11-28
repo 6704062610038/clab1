5ef8acba1b72b4662cb5b238b380638c4329d58f
6704062610038 <s6704062610038@email.kmutnb.ac.th>
2024-11-28 14:41:33 +0700

    Add files via upload

--- lab1minx.c
+++ lab1minx.c
@@ -3,8 +3,7 @@ int main(){
 	int num1;
 	int num2;
 	int a;
-	scanf("%d",&num1);
-	scanf("%d",&num2);
+	scanf("%d %d",&num1,&num2);
 	if(num1>num2){
 		a=num1-num2;
 		printf("num1 have %d point more than num2",a);
